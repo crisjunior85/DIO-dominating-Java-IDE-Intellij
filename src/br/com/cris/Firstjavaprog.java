947d233f4f08729e5d6d0fb7ee89a82719093870
crisjunior85 <kuyakito@gmail.com>
2022-01-23 12:10:38 -0300

    first commit

---
+++ src/br/com/cris/Firstjavaprog.java
@@ -0,0 +1,15 @@
+package br.com.cris;
+
+import br.com.cris.model.Cat;
+
+public class Firstjavaprog {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+
+        System.out.println(cat);
+
+        int a = 5;
+        int b = 3;
+        System.out.println("hi world " + (a+b));
+    }
+}
